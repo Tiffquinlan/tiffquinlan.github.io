93037f0cc94c38e0d8ed86695412fb6b0970d0c2
tiffquinlan <tiffquinlan@gmail.com>
2020-05-21 20:03:19 -0700

    initial lesson 5

---
+++ lesson4 copy/js/javascript.js
@@ -0,0 +1,25 @@
+    const hambutton = document.querySelector('.ham');
+    const mainnav = document.querySelector('.navigation')
+
+    hambutton.addEventListener('click', () => {
+        mainnav.classList.toggle('responsive')
+    }, false);
+
+
+    var date = new Date();
+    var d = new Date();
+    // document.getElementById("day").innerHTML = d.getDay();
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = ["January","February","March","April","May","June","July","August","September","October","November","December"
+    ];
+    document.getElementById("day").innerHTML = days[d.getDay()];
+    year = d.getFullYear();
+    month = months[d.getMonth()];
+    DayNum = d.getDate();
+    date= DayNum + " " + month + " " + year;
+    document.getElementById('date').textContent = date;
+
+    function myFunction(x) {
+        x.classList.toggle("change");
+
+    }
