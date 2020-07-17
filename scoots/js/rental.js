7b794c2401602bc68abb5e7ed2af812d8e0b4504
tiffquinlan <tiffquinlan@gmail.com>
2020-07-16 22:47:10 -0700

    card fix

--- scoots/js/rental.js
+++ scoots/js/rental.js
@@ -20,7 +20,10 @@ fetch(requestURL)
                 let type = document.createElement('h3');
                 let description = document.createElement('h3');
                 let people = document.createElement('h3');
-                let prices = document.createElement('h3')
+                let price1 = document.createElement('p')
+                let price2 = document.createElement('p')
+                let price3 = document.createElement('p')
+                let price4 = document.createElement('p')
                 let image = document.createElement('img');
 
 
@@ -29,15 +32,25 @@ fetch(requestURL)
                 console.log("name " + rentals[i].name);
                 description.textContent = rentals[i].description + "\n";
                 people.textContent = "Max Persons " + rentals[i].people + "\n";
-                prices.textContent = rentals[i].reservation[0].time + " " + rentals[i].reservation[0].price;
+
+                price1.textContent = "Prices starting at " + rentals[i].reservation[0].price +" / "+ rentals[i].reservation[0].time  ;
+
+                // price1.textContent = rentals[i].reservation[0].time + " " + rentals[i].reservation[0].price;
+                // price2.textContent = rentals[i].reservation[1].time + " " + rentals[i].reservation[1].price;
+                // price3.textContent = rentals[i].walkin[0].time + " " + rentals[i].walkin[0].price;
+                // price4.textContent = rentals[i].walkin[1].time + " " + rentals[i].walkin[1].price;
+                
                 image.setAttribute('src', "images/" + rentals[i].photo);
                 image.setAttribute('alt', rentals[i].name + ' picture');
 
                 // info.appendChild(type);
                 info.appendChild(h1);
-                // info.appendChild(description);
-                // info.appendChild(people);
-                info.appendChild(prices);
+                info.appendChild(description);
+                info.appendChild(people);
+                info.appendChild(price1);
+                // info.appendChild(price2);
+                // info.appendChild(price3);
+                // info.appendChild(price4);
 
                 item.appendChild(info);
                 item.appendChild(image);
